73dc98e64f2c7545a812bc18ccc18990a5803b12
Daniel Henrique <danielhmarinho@gmail.com>
2016-04-12 19:28:35 -0300

    Implementing  tests for models.py
    Signed-off-by: Daniel Henrique <danielhmarinho@gmail.com>
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- test/test_models.py
+++ test/test_models.py
@@ -3,6 +3,7 @@ import pet.models
 from sqlalchemy.exc import OperationalError
 import os.path
 from mock import MagicMock
+import pet.vcs
 
 
 class TestPetModels(unittest.TestCase):
@@ -41,3 +42,30 @@ class TestPetModels(unittest.TestCase):
             engine = pet.models.check_ssl_certificate()
         self.assertEquals("(NoneType) None",cm.exception.message)
 
+    def test_get_col_spec(self):
+        debversion = pet.models.DebVersion()
+        self.assertEqual("DEBVERSION", debversion.get_col_spec())
+
+    def test_bind_processor(self):
+        debversion = pet.models.DebVersion()
+        process = debversion.bind_processor(None)
+        value = 123
+        self.assertEqual(process(value), value)
+
+    def test_result_processor(self):
+        debversion = pet.models.DebVersion()
+        process = debversion.result_processor(None, None)
+        value = 123
+        self.assertEqual(process(value), value)
+
+    def test_vcs_if_vcs_exists(self):
+        repository = pet.models.Repository()
+        value = 123
+        repository._vcs = value
+        self.assertEqual(repository.vcs, value)
+
+    def test_vcs_not_in_dict(self):
+        repository = pet.models.Repository()
+        value = 'git'
+        pet.vcs.vcs_backend = MagicMock(return_value=value)
+        self.assertEqual(repository.vcs, value)
