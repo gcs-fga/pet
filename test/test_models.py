99bb7eb5a470e01936ebd321d6a00c49cf121661
Lucas Severo <lucassalves65@gmail.com>
2016-05-03 19:50:32 -0300

    ssh fix, exceptions and tests
    	* removed question for ssl certificate use
    	* added exceptions
    	* tests
    
    Signed-off-by: Lucas Severo <lucassalves65@gmail.com>
    
    tests for ssl cert flag
    
    Signed-off-by: Lucas Severo <lucassalves65@gmail.com>

---
+++ test/test_models.py
@@ -0,0 +1,44 @@
+import unittest
+import pet.models
+import pet
+from sqlalchemy.exc import OperationalError
+import os.path
+from mock import MagicMock
+
+
+class TestPetModels(unittest.TestCase):
+
+    def setUp(self):
+        self.isfile = os.path.isfile
+        self.engine = pet.engine
+
+    def tearDown(self):
+        os.path.isfile = self.isfile
+        pet.engine = self.engine
+
+    def test_check_ssl_certificate_with_file(self):
+        os.path.isfile = MagicMock(return_value=True)
+        pet.engine = MagicMock(return_value=True)
+        engine = pet.models.check_ssl_certificate()
+        self.assertIsNotNone(engine)
+
+    def test_check_ssl_certificate_with_file_ex(self):
+        os.path.isfile = MagicMock(return_value=True)
+        pet.engine = MagicMock(side_effect=OperationalError("", {}, None))
+        with self.assertRaises(OperationalError) as cm:
+            engine = pet.models.check_ssl_certificate()
+        self.assertEquals("(NoneType) None",cm.exception.message)
+
+    def test_check_ssl_certificate_no_file(self):
+        os.path.isfile = MagicMock(return_value=False)
+        pet.engine = MagicMock(return_value=True)
+        engine = pet.models.check_ssl_certificate()
+        self.assertIsNotNone(engine)
+
+    def test_check_ssl_certificate_no_file_ex(self):
+        os.path.isfile = MagicMock(return_value=False)
+        pet.engine = MagicMock(side_effect=OperationalError("", {}, None))
+        with self.assertRaises(OperationalError) as cm:
+            engine = pet.models.check_ssl_certificate()
+        self.assertEquals("(NoneType) None",cm.exception.message)
+
