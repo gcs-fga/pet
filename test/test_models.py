3b7d1d7aa1c9c091d26ea75fd350b2910be88258
CaioIcy <caio.nardelli@hotmail.com>
2016-05-03 19:18:19 -0300

    Finish testing models
    Signed-off-by: Caio Nardelli <caio.nardelli@hotmail.com>
    Signed-off-by: Daniel Henrique <danielhmarinho@gmail.com>
    Signed-off-by: Victor <victorfgcabeceira@gmail.com>

--- test/test_models.py
+++ test/test_models.py
@@ -135,3 +135,58 @@ class TestPetModels(unittest.TestCase):
         expected = self.ntNone
         actual = pet.models.Package().trunk
         self.assertEqual(expected, actual)
+
+    def test_models_tablenames(self):
+        self.assertEqual('config', pet.models.Config.__tablename__)
+        self.assertEqual('team', pet.models.Team.__tablename__)
+        self.assertEqual('repository', pet.models.Repository.__tablename__)
+        self.assertEqual('package', pet.models.Package.__tablename__)
+        self.assertEqual('named_tree', pet.models.NamedTree.__tablename__)
+        self.assertEqual('watch_result', pet.models.WatchResult.__tablename__)
+        self.assertEqual('wait', pet.models.Wait.__tablename__)
+        self.assertEqual('file', pet.models.File.__tablename__)
+        self.assertEqual('patch', pet.models.Patch.__tablename__)
+        self.assertEqual('archive', pet.models.Archive.__tablename__)
+        self.assertEqual('suite', pet.models.Suite.__tablename__)
+        self.assertEqual('suite_package', pet.models.SuitePackage.__tablename__)
+        self.assertEqual('suite_binary', pet.models.SuiteBinary.__tablename__)
+        self.assertEqual('bug_tracker', pet.models.BugTracker.__tablename__)
+        self.assertEqual('bug', pet.models.Bug.__tablename__)
+        self.assertEqual('bug_source', pet.models.BugSource.__tablename__)
+
+    def test_named_tree__file(self):
+        value = 2052016
+        # Mocking chained methods
+        query_rv = MagicMock()
+        query_rv.filter_by = MagicMock(return_value=value)
+        object_session_rv = MagicMock()
+        object_session_rv.query = MagicMock(return_value=query_rv)
+        pet.models.Session.object_session = MagicMock(return_value=object_session_rv)
+        self.assertEqual(value, self.nt1._file('filename'))
+
+    def test_named_tree_has_file_zero(self):
+        mock_count = MagicMock()
+        mock_count.count = MagicMock(return_value=0)
+        self.nt1._file = MagicMock(return_value=mock_count)
+        self.assertEqual(False, self.nt1.has_file('filename'))
+
+    def test_named_tree_has_file_nonzero(self):
+        mock_count = MagicMock()
+        mock_count.count = MagicMock(return_value=100000)
+        self.nt1._file = MagicMock(return_value=mock_count)
+        self.assertEqual(True, self.nt1.has_file('filename'))
+
+    def test_named_tree_file(self):
+        value = 20520162
+        mock_one = MagicMock()
+        mock_one.one = MagicMock(return_value=value)
+        self.nt1._file = MagicMock(return_value=mock_one)
+        self.assertEqual(value, self.nt1.file('filename'))
+
+    def test_named_tree_link(self):
+        value = 20520163
+        self.nt1.package = MagicMock()
+        self.nt1.package.repository = MagicMock()
+        self.nt1.package.repository.vcs = MagicMock()
+        self.nt1.package.repository.vcs.link = MagicMock(return_value=value)
+        self.assertEqual(value, self.nt1.link('filename'))
