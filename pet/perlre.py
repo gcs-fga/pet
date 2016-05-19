41ec83b803ea4083210c16213004432f7170cd37
Victor <victorfgcabeceira@gmail.com>
2016-05-19 17:27:21 -0300

    Refactoring perlre
    Signed-off-by: Victor <victorfgcabeceira@gmail.com>
    Signed-off-by: Tiago <thiagoribeironiquel@hotmail.com>
    Signed-off-by: Lucas <lucassalves65@gmail.com>

--- pet/perlre.py
+++ pet/perlre.py
@@ -58,70 +58,98 @@ _replacement_rules = [
     (re.compile(r'\$\{?(\d)\}?'), r'\\g<\1>'),
 ]
 
+PATTERN = 0
+REPLACEMENT_MARKER = 1
+REPLACEMENT = 2
+FLAGS = 3
+
 
 def compile(pattern):
+    """Compile a regular expression pattern into a regular expression object,
+    which can be used for matching using its match() and search() methods."""
     for regex, sub in _pattern_rules:
         pattern = regex.sub(sub, pattern)
     return re.compile(pattern)
 
 
-def apply_perlre(regexp, string):
+def not_null_regex(regexp, string):
+    """Verify if the regex is not null; if False, return string."""
     regexp = regexp.strip()
     if regexp == "":
-        return string
+        regex_is_not_null = False
+    else:
+        regex_is_not_null = True
 
+    return regex_is_not_null
+
+
+def operator_matching(regexp, string):
+    """Verify if the regex is valid, if not, raise regexp RegexpError."""
     match_op = _re_op.match(regexp)
     if not match_op:
         raise RegexpError(
-            "Unknown operator in regular expression '{0}'.".format(regexp))
+            "Unknown operator in regular expression '{0}'.".format(regexp)
+        )
     op = match_op.group(1)
 
     if op != 's':
         raise NotImplemented("Operator '{0}' not implemented.".format(op))
 
+    return match_op
+
+
+def initial_decoding(regexp, string):
+    """State machine."""
+    match_op = operator_matching(regexp, string)
     arguments = match_op.group(2)
     marker = arguments[0]
     end_marker = _markers.get(marker, marker)
     last_was_escape = False
 
-    stage = 0  # 0=pattern, (1=replacement-marker), 2=replacement, 3=flags
+    stage = PATTERN
     pattern = replacement = flags = ""
     for char in arguments[1:]:
-        if stage == 1:
+        if stage == REPLACEMENT_MARKER:
             end_marker = _markers.get(char, char)
-            stage = 2
+            stage = REPLACEMENT
             continue
         if last_was_escape:
             last_was_escape = False
-            if stage == 0:
+            if stage == PATTERN:
                 pattern += char
-            elif stage == 1:
+            elif stage == REPLACEMENT_MARKER:
                 raise RegexpError("Invalid regular expression.")
-            elif stage == 2:
+            elif stage == REPLACEMENT:
                 replacement += char
             else:
                 flags += char
         elif char == end_marker:
-            if stage == 0:
+            if stage == PATTERN:
                 if marker != end_marker:
-                    stage = 1
+                    stage = REPLACEMENT_MARKER
                 else:
-                    stage = 2
+                    stage = REPLACEMENT
             else:
-                stage = 3
+                stage = FLAGS
         else:
             if char == "\\":
                 last_was_escape = True
-            if stage == 0:
+            if stage == PATTERN:
                 pattern += char
-            elif stage == 2:
+            elif stage == REPLACEMENT:
                 replacement += char
             else:
                 flags += char
 
-    if stage != 3:
+    if stage != FLAGS:
         raise RegexpError("Invalid regular expression.")
 
+    return pattern, replacement, flags
+
+
+def flag_decoding(regexp, string):
+    """Defines the count variable."""
+    flags = initial_decoding(regexp, string)[2]
     count = 1
     py_flags = 0
     for flag in flags:
@@ -132,15 +160,46 @@ def apply_perlre(regexp, string):
         else:
             raise RegexpError(
                 "Unknown flag '{0}' used in regular expression.".format(flags))
+    return count
 
+
+def regex_in_rules(regexp, string):
+    """Defines the pattern and replacement / replace with replacement after
+    every occurrence of pattern."""
+    pattern = initial_decoding(regexp, string)[0]
+    replacement = initial_decoding(regexp, string)[1]
     for regex, sub in _pattern_rules:
         pattern = regex.sub(sub, pattern)
     for regex, sub in _replacement_rules:
         replacement = regex.sub(sub, replacement)
 
-    # TODO: flags is only in Python 2.7
+    return pattern, replacement
+
+
+def apply_substitute_regex(pattern, replacement, string, count):
+    """Apply pattern and replacement on the string and return."""
     try:
-        return re.sub(pattern, replacement, string, count=count)
+        string_regex_applied = re.sub(
+            pattern, replacement, string, count=count
+        )
     except:
-        return string
-    # return re.sub(pattern, replacement, string, count=count, flags=py_flags)
+        string_regex_applied = string
+
+    return string_regex_applied
+
+
+def apply_perlre(regexp, string):
+    """Method to call all other methods."""
+    empty_regex = not_null_regex(regexp, string)
+    if empty_regex:
+        operator_matching(regexp, string)
+        pattern = regex_in_rules(regexp, string)[0]
+        replacement = regex_in_rules(regexp, string)[1]
+        count = flag_decoding(regexp, string)
+        string_regex_applied = apply_substitute_regex(
+            pattern, replacement, string, count
+        )
+    else:
+        string_regex_applied = string
+
+    return string_regex_applied
