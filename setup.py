91f923201e8989371b1feb87803d54f7bcbe7a77
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-06-24 13:25:58 -0300

    Update packing setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.61',
+    version='1.2.62',
 
     description='PET is a collection of scripts that gather informatio about your packages',
     long_description=long_description,
