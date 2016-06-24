ca11a95ee40ad3c8df41721507578922aadb3d90
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-06-24 14:03:49 -0300

    Update packing setup.py

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 all:
 	-mv -f dist/* log
-	python2.7 setup.py sdist
-	python setup.py bdist_wheel --universal
+	python setup.py sdist
+	python setup.py bdist_wheel
+	# python setup.py bdist_wheel --universal
 	twine upload dist/*
 	mkdir -p log
 	mv dist/* log
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.66',
+    version='1.2.68',
 
     description='PET is a collection of scripts that gather informatio about your packages',
     long_description=long_description,
